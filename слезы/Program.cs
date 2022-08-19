0a3848436ffb3afaa5c98519ff2490b39dc34126
KET <varlamova_e.t@mail.ru>
2022-08-19 21:18:15 +0300

    я в шоке

---
+++ слезы/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("a - максимальное число");
+}
+else
+{
+    Console.WriteLine("b - максимальное число");
+}
